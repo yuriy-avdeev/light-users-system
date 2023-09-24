6dbbd1d527084b8492b83d2259447bb3b7dd6c8e
yuriy-avdeev <y.avdeiev@gmail.com>
2023-09-24 14:01:30 +0300

    refactor:  clean up preset structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// defineProps<{
+//   navItems: array;
+// }>();
+</script>
+
+<template>
+  <header class="header">body od header</header>
+</template>
+
+<style scoped>
+/*  */
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../pages/HomePage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +7,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomePage,
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      // route is lazy-loaded when the route is visited
+      component: () => import('../pages/AboutPage.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
