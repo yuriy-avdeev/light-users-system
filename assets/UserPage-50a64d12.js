861a75eafd75e436be6929b0f961becd346d0b4d
yuriy-avdeev <y.avdeiev@gmail.com>
2023-11-12 22:08:06 +0200

    Updates

---
+++ assets/UserPage-50a64d12.js
@@ -0,0 +1,1 @@
+import{d as a,o as t,c as s,a as r,t as o}from"./index-1698f076.js";const i=a({__name:"UserPage",setup(n){return(e,p)=>(t(),s("div",null,[r("h2",null,"User with id - "+o(e.$route.params.id)+" page test",1)]))}});export{i as default};
