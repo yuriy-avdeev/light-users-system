68f551770b000e39187c3c95424bc65561ab01d2
yuriy-avdeev <y.avdeiev@gmail.com>
2024-01-21 16:47:24 +0200

    Updates

--- assets/AboutPage-15c1e5da.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o as t,c as o,a}from"./index-4e9ff369.js";const _={class:"about-page"},s=a("h1",{class:"about-page__title"},"About page",-1),c=[s],d=e({__name:"AboutPage",setup(n){return(p,u)=>(t(),o("div",_,c))}});export{d as default};
